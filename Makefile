09b0f464091b553fda6c2b87d5611cc6b3510e7c
Antonin Deniau <adeniau@afoneinfrastructure.com>
2020-08-11 16:52:56 +0200

    almost

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ clean:
 
 compile:
 	mkdir -p ./dist
-	cat templates/template.js |sed "s/STRING_TO_REPLACE/$$(./vm_compile ./src/main.jasm)/" | terser --compress --mangle -- > dist/index.js
-	D=$$(cat ./dist/index.js|sed 's/\&/\\&/g'); cat ./templates/template.htm | sed "s/STRING_TO_REPLACE/$${D}/" | html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true > ./dist/index.html
+	cat templates/template.js |sed "s/STRING_TO_REPLACE/$$(./vm_compile ./src/main.jasm)/" | npx terser --compress --mangle -- > dist/index.js
+	D=$$(cat ./dist/index.js|sed 's/\&/\\&/g'); cat ./templates/template.htm | sed "s/STRING_TO_REPLACE/$${D}/" | npx html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true > ./dist/index.html
 	cat ./dist/index.html | lzma -9 | base64 | xargs -0 printf "https://itty.bitty.site/#/%s\n" |tr -d \\n
 
--- templates/template.js
+++ templates/template.js
@@ -1,25 +1,24 @@
 a=STRING_TO_REPLACE;
-i=0;
+i=1;
 x=[
 	x=>alert`BadPass.`,
-	x=>x.push(...prompt().split`,`.map(x=>+x)),
-	x=>x[50]=8,
+	x=> x.push(...prompt().split``.map(f=>f.charCodeAt(0))),
 	x=>alert`Good pass !`,
 ];
 
-while(a[50]!=8){
-    console.log(a[i], a[i+1], a[i+2]); // TODO remove
-    [0,
-	_=>a[47]==1?i=a[i+1]:i++,    /*1 jeq <addr> TEST [47] */
-	_=>x[a[++i]](a),             /*2 int <num> */
-	_=>a[i+1]=a[++i]^a[++i],     /*3 xor <a> <b> TO <a> */
-	_=>a[a[++i]]=a[a[++i]],      /*4 mov <a> <b> */
-	_=>a[a[i+1]]=a[a[++i]]+1,    /*5 inc <a> */
-	_=>a[a[++i]]=a[++i],         /*6 set <a> <b> */
-	_=>i=a[++i],                 /*7 jmp <a> */
-	_=>a[47]=a[++i]==a[++i]?1:0, /*8 test <a> <b> TO [47] */
+while (a[0]!=5) {
+    console.log(`${i} ${a[i]}: ${a[i+1]} => ${a[a[i+1]]} ${a[i+2]} => ${a[a[i+2]]}`);
+    [
+      _=>a[0]==1?i=a[++i]-1:i++,        // 1 jeq <addr> TEST [47]
+      _=>x[a[++i]](a),                  // 2 int <num>
+      _=>a[i+1]=a[a[++i]]^a[a[++i]],    // 3 xor <a> <b> TO <a>
+      _=>a[a[++i]]=a[a[++i]],           // 4 mov <a> <b>
+      _=>++a[a[++i]],                   // 5 inc <a>
+      _=>a[a[++i]]=a[++i],              // 6 set <a> <b>
+      _=>i=a[1+i]-1,                    // 7 jmp <a>
+      _=>a[0]=a[a[++i]]==a[a[++i]]?1:0, // 8 test <a> <b> TO [47]
     ][a[i]]()
 
-	i++;
+    i++;
     if(i==50) break; // TODO remove
 }
