4e07355d3096a20d29bc55b63b7eb35da4691de6
Antonin Deniau <antonin.deniau@protonmail.com>
2020-08-11 20:48:13 +0200

    working

--- templates/template.js
+++ templates/template.js
@@ -7,18 +7,15 @@ x=[
 ];
 
 while (a[0]!=5) {
-    //console.log(`${i} ${a[i]}: ${a[i+1]} => ${a[a[i+1]]} ${a[i+2]} => ${a[a[i+2]]}`);
     [
-      _=>a[0]==1?i=a[++i]-1:i++,        // 1 jeq <addr> TEST [0]
-      _=>x[a[++i]](a),                  // 2 int <num>
-      _=>a[i+1]=a[a[++i]]^a[a[++i]],    // 3 xor <a> <b> TO <a>
-      _=>a[a[++i]]=a[a[++i]],           // 4 mov <a> <b>
-      _=>++a[a[++i]],                   // 5 inc <a>
-      _=>a[a[++i]]=a[++i],              // 6 set <a> <b>
-      _=>i=a[1+i]-1,                    // 7 jmp <a>
-      _=>a[0]=a[a[++i]]==a[a[++i]]?1:0, // 8 test <a> <b> TO [0]
-    ][a[i]]()
-
+      _=>a[0]==1?i=a[++i]-1:i++,        // 0 jeq <addr> TEST [0]
+      _=>x[a[++i]](a),                  // 1 int <num>
+      _=>a[i+1]=a[a[++i]]^a[a[++i]],    // 2 xor <a> <b> TO <a>
+      _=>a[a[++i]]=a[a[++i]],           // 3 mov <a> <b>
+      _=>++a[a[++i]],                   // 4 inc <a>
+      _=>a[a[++i]]=a[++i],              // 5 set <a> <b>
+      _=>i=a[1+i]-1,                    // 6 jmp <a>
+      _=>a[0]=a[a[++i]]==a[a[++i]]?1:0, // 7 test <a> <b> TO [0]
+    ][a[i]]();
     i++;
-    //if(i==50) break; // TODO remove
 }
